7a833e7a379c8e62d7f629e6f66b36cd8cba01e3
ThinkCat <624432528@qq.com>
2016-10-20 21:51:24 +0800

    remove comments

--- app.py
+++ app.py
@@ -18,7 +18,5 @@ def error500(error):
     return 'server error'
 
 if __name__ == '__main__':
-    #App.merge(AppView)
-    #App.merge(Api)
     #serve(App,host='0.0.0.0',port=8000)
     App.run(host='localhost', port=8000, debug=True, reloader=True)
